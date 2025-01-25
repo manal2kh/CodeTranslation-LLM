dba4af8cbb526fbff737dcd6046c2c45d7c4097f
manal <44410645+manal2kh@users.noreply.github.com>
2025-01-25 18:09:58 +0300

    Update pipeline.py

--- sec/pipeline.py
+++ sec/pipeline.py
@@ -1,1 +1,12 @@
+import subprocess
+
+
+print("Starting refactoring process ...")
+subprocess.run(['python', 'refactoring.py'])
+
+
+print("Starting translation process ...")
+subprocess.run(['python', 'CodeTranslationLLM.py'])
+
+print("Pipeline completed successfully!")
 
